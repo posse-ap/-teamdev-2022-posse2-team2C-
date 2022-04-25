4c19e13894e6e6d7000ae8d2b8545a54439ba546
Kanta Ono <kanta20021476@gmail.com>
2022-04-25 15:58:23 +0900

    もう一個あった

--- src/shukatu/agent_login/agent_login_forget.php
+++ src/shukatu/agent_login/agent_login_forget.php
@@ -9,7 +9,7 @@ if(isset($_SESSION["login"]) === false) {
     print "登録しているメールアドレス ";?>
     <form action=agent_login_check.php method=post>
     <input type="text">
-</form>
+    </form>
 <?php
     exit();
 } else {
@@ -19,16 +19,7 @@ if(isset($_SESSION["login"]) === false) {
 
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body> 
 
 
-</body>
-</html>
+
+
